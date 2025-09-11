7c49fc065455c6da669766b3077e5b3e6e49cd91
Arfaj-sheru <145114153+arfajsheru@users.noreply.github.com>
2025-09-11 15:57:29 +0530

    remove sitemeasurment view modal remove and dynamic route added and handled designingstate button handled and pilltabs me designs tab me empty handled kiya

--- src/hooks/Site-measruement/useSiteMeasruementLeadsQueries.ts
+++ src/hooks/Site-measruement/useSiteMeasruementLeadsQueries.ts
@@ -1,6 +1,5 @@
-import { getInitialSiteMeasurement2 } from "@/api/measurment-leads";
-import { SiteMeasurmentResponse } from "@/types/site-measrument-types";
-import { useQuery, UseQueryResult } from "@tanstack/react-query";
+import { getInitialSiteMeasurement } from "@/api/measurment-leads";
+import { useQuery } from "@tanstack/react-query";
 
 export const useInitialSiteMeasurement = (
   vendorId: number,
@@ -8,8 +7,8 @@ export const useInitialSiteMeasurement = (
   enabled: boolean = true
 ) => {
   return useQuery({
-    queryKey: ["siteMeasurementLeads"],
-    queryFn: () => getInitialSiteMeasurement2(vendorId, statusId),
+    queryKey: ["siteMeasurementLeads", vendorId, statusId],
+    queryFn: () => getInitialSiteMeasurement(vendorId, statusId),
     enabled: enabled && !!vendorId,
     staleTime: 5 * 60 * 1000,
     refetchOnWindowFocus: false,
