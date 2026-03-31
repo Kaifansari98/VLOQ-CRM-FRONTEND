f23e673824f336cf0154275de2cacbce576879d7
CrestWebStudios <154508851+Kaifansari98@users.noreply.github.com>
2026-03-31 12:04:42 +0530

    done

--- src/components/team-switcher.tsx
+++ src/components/team-switcher.tsx
@@ -10,7 +10,6 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
