86f461ffc3845c6820062951876dcc21a0b2a6fe
arfaj.sheru <arfatsheru74@gmail.com>
2026-07-13 16:12:55 +0530

    feat: exclude other franchise admins from assignable users

--- src/components/sales-executive/Lead/assign-task-final-measurement-form.tsx
+++ src/components/sales-executive/Lead/assign-task-final-measurement-form.tsx
@@ -504,7 +504,7 @@ const AssignTaskFinalMeasurementForm: React.FC<Props> = ({
       if (leadFranchiseId !== undefined && leadFranchiseId !== null) {
         if (
           user.franchise_id !== leadFranchiseId &&
-          normalizedAssignableUserType === "sales-executive"
+          ["sales-executive", "admin"].includes(normalizedAssignableUserType)
         ) {
           return false;
         }
